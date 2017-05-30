fc77f85c89877cf166eb1920bdc36af44977b22a
dhg <dhgarrette@gmail.com>
2017-05-30 01:27:14 -0400

    improvements

--- make_transcription_website.py
+++ make_transcription_website.py
@@ -2,7 +2,8 @@ import codecs
 import os
 
 output_dir = "/Users/dhg/workspace/symposiumPrep/train_output_17-05-24"
-website_dir = "/Users/dhg/workspace/halperta/firstbooks/transcriptions"
+website_dir = "/Users/dhg/workspace/halperta/firstbooks"
+transcriptions_dir = "%s/transcriptions" % (website_dir)
 website_image_size = "800"  # 400, 800, 1000
 
 
@@ -27,8 +28,8 @@ with codecs.open("%s/index.html" % (website_dir), 'w', encoding='utf8') as index
     if book_name[0] != '.':  # skip hidden files
       if book_name not in all_pages:
         all_pages[book_name] = []
-      if not os.path.exists("%s/%s" % (website_dir, book_name)):
-        os.makedirs("%s/%s" % (website_dir, book_name))
+      if not os.path.exists("%s/%s" % (transcriptions_dir, book_name)):
+        os.makedirs("%s/%s" % (transcriptions_dir, book_name))
       book_dir = '%s/%s/all_transcriptions/1000/' % (output_dir, book_name)
       for filename in os.listdir(book_dir):
         # pl_blac_012_00019-1000_iter-3_transcription_normalized.txt
@@ -37,14 +38,14 @@ with codecs.open("%s/index.html" % (website_dir), 'w', encoding='utf8') as index
           page_name = filename[:-len(suffix)]
           all_pages[book_name].append(page_name)
       for (i, page_name) in enumerate(all_pages[book_name]):
-        output_filename = "%s/%s/%s.html" % (website_dir, book_name, page_name)
+        output_filename = "%s/%s/%s.html" % (transcriptions_dir, book_name, page_name)
         print 'Writing %s' % (output_filename)
         with codecs.open(output_filename, 'w', encoding='utf8') as fout:
           if i > 0:
-            fout.write('<a href="%s/%s/%s.html">Prev</a>&nbsp;\n' % (website_dir, book_name, all_pages[book_name][i-1]))
-          fout.write('<a href="%s/%s/index.html">Up</a>&nbsp;\n' % (website_dir, book_name))
+            fout.write('<a href="%s/%s/%s.html">Prev</a>&nbsp;\n' % (transcriptions_dir, book_name, all_pages[book_name][i-1]))
+          fout.write('<a href="%s/%s/index.html">Up</a>&nbsp;\n' % (transcriptions_dir, book_name))
           if i < len(all_pages[book_name])-1:
-            fout.write('<a href="%s/%s/%s.html">Next</a>\n' % (website_dir, book_name, all_pages[book_name][i+1]))
+            fout.write('<a href="%s/%s/%s.html">Next</a>\n' % (transcriptions_dir, book_name, all_pages[book_name][i+1]))
           fout.write('<br/>\n')
 
           fout.write('<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">\n')
@@ -59,10 +60,10 @@ with codecs.open("%s/index.html" % (website_dir), 'w', encoding='utf8') as index
           fout.write('</td></tr></table>\n')
           fout.write('</body>\n')
           fout.write('</html>\n')
-      with codecs.open("%s/%s/index.html" % (website_dir, book_name), 'w', encoding='utf8') as fout:
+      with codecs.open("%s/%s/index.html" % (transcriptions_dir, book_name), 'w', encoding='utf8') as fout:
         fout.write('<a href="%s/index.html">Home</a><br/><br/>\n' % (website_dir))
         for page_name in all_pages[book_name]:
-          fout.write('<a href="%s/%s/%s.html">%s</a><br/>\n' % (website_dir, book_name, page_name, page_name))
-      index_fout.write('<a href="%s/%s/index.html">%s</a><br/>\n' % (website_dir, book_name, book_name))
+          fout.write('<a href="%s/%s/%s.html">%s</a><br/>\n' % (transcriptions_dir, book_name, page_name, page_name))
+      index_fout.write('<a href="%s/%s/index.html">%s</a><br/>\n' % (transcriptions_dir, book_name, book_name))
 
 
