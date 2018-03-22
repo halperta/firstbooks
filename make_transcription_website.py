99d17143a4d0b5d5fc9410db05ad429fc9f97e89
Alpert-Abrams <hra288@lib-sr1am002.lib.utexas.edu>
2018-03-22 16:12:59 -0500

    updating book names

--- make_transcription_website.py
+++ make_transcription_website.py
@@ -15,7 +15,15 @@ github_dir = "/Users/hra288/Github/neh/firstbooks/"
 website = "http://www.halperta.com/firstbooks"
 transcriptions_dir = "%s/finalTranscriptions" % (github_dir)
 website_image_size = "800"  # 400, 800, 1000
-csvPath = "/Users/hra288/Github/neh/pl-works-11-10-16.csv"
+csvPath = "/Users/hra288/Github/neh/pl-works_newdata_170314.csv"
+
+
+# output_dir = "/Users/hra288/Github/neh/testTranscribedBooks"
+# github_dir = "/Users/hra288/Github/neh/testfirstbooks/"
+# website = "http://www.halperta.com/testfirstbooks"
+# transcriptions_dir = "%s/finalTranscriptions" % (github_dir)
+# website_image_size = "800"  # 400, 800, 1000
+# csvPath = "/Users/hra288/Github/neh/pl-works_newdata_170314.csv"
 
 
 all_pages = dict()  # all_pages[book_name] = page_names
@@ -27,6 +35,9 @@ with open(csvPath) as csvfile:
     metadata_dict[row['wks_book_id']] = row 
 
 with codecs.open("%s/index.html" % (github_dir), 'w', encoding='utf8') as index_fout:
+  # fout.write('<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">\n')
+  # fout.write('<head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/></head>\n')
+  # fout.write('<body>\n')
   index_fout.write('<font size=+2>Reading the First Books</font>')
   index_fout.write('<br/>\n')
   index_fout.write('<br/>\n')
@@ -41,6 +52,22 @@ with codecs.open("%s/index.html" % (github_dir), 'w', encoding='utf8') as index_
   index_fout.write("""
    For more information about the project or the data, contact <a href="http://www.halperta.com">Hannah Alpert-Abrams</a> (halperta@gmail.com).
     <br/><br/>\n""")
+  index_fout.write('<div>')
+  index_fout.write("""
+    <script>
+      (function() {
+        var cx = '012497753802185401351:krmz1a5vprq';
+        var gcse = document.createElement('script');
+        gcse.type = 'text/javascript';
+        gcse.async = true;
+        gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(gcse, s);
+      })();
+    </script>
+    <gcse:search></gcse:search> 
+  """)
+  # index_fout.write('<\div>')
   index_fout.write('<p style="margin-left: 40px">')
 
   if not os.path.exists("%s" % (transcriptions_dir)):
@@ -70,10 +97,6 @@ with codecs.open("%s/index.html" % (github_dir), 'w', encoding='utf8') as index_
       for (i, page_name) in enumerate(all_pages[book_name]):
         print('Writing %s/%s/%s.html' % (transcriptions_dir, book_name, page_name))
         with codecs.open("%s/%s/%s.html" % (transcriptions_dir, book_name, page_name), 'w', encoding='utf8') as fout:
-          fout.write('<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">\n')
-          fout.write('<head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/></head>\n')
-          fout.write('<body>\n')
-
           if i > 0:
             fout.write('<a href="%s.html">Prev</a>&nbsp;\n' % (all_pages[book_name][i-1]))
           fout.write('<a href="index.html">Up</a>&nbsp;\n' % ())
@@ -98,7 +121,6 @@ with codecs.open("%s/index.html" % (github_dir), 'w', encoding='utf8') as index_
           fout.write('</td><td>\n')
           fout.write('<img src="http://primeroslibros.org/primeros_media/%s/%s/%s-%s.jpg">\n' % (book_name, website_image_size, page_name, website_image_size))
           fout.write('</td></tr></table>\n')
-          fout.write('</body>\n')
           fout.write('</html>\n')
       with codecs.open("%s/%s/index.html" % (transcriptions_dir, book_name), 'w', encoding='utf8') as fout:
         fout.write('<a href="../../index.html">Home</a><br/><br/>\n' % ())
@@ -113,5 +135,5 @@ with codecs.open("%s/index.html" % (github_dir), 'w', encoding='utf8') as index_
         for page_name in all_pages[book_name]:
           fout.write('<a href="%s.html">%s</a><br/>\n' % (page_name, page_name))
       index_fout.write('<a href="finalTranscriptions/%(book_name)s/index.html">%(bookTitle)s (%(bookPrinter)s, %(bookDate)s)</a><br/>\n' % {"book_name": book_name, "bookTitle": bookTitle, "bookPrinter": bookPrinter, "bookDate": bookDate})
-
+      index_fout.write('</body>\n')
 
